e9974c704fbfbb009175feea7c7bea70b0956237
Andrey Reinwald <andreinwald@gmail.com>
2016-04-01 18:01:00 +0300

    Added send email to example.php

--- example.php
+++ example.php
@@ -1,13 +1,40 @@
 <?php
+require_once 'autoloader.php';
 
-// If you no have autoloader
-foreach (glob('mailfire/*.php') as $filename) {
-    require_once $filename;
-}
+// Init Mailfire SDK object
+$clientId = 123;
+$clientHash = 'a1s2d3f4g5h6j7k8l';
+$mf = new \Mailfire\Mailfire($clientId, $clientHash);
 
-// Creating Mailfire object
-$mf = new Mailfire(1, 2);
 
-// Send letter
-$res = $mf->push->send(1, 2, 3, 4, 5);
-var_dump($res);
+// Send email
+$typeId = 1; // letter id
+$categoryId = $mf->push->getCategorySystem(); // system or trigger
+$projectId = 1;
+$email = 'test@example.com';
+
+$user = [ // User info, if you know
+    'name' => 'John',
+    'age' => '22',
+    'gender' => 'm',
+    'language' => 'en',
+    'country' => 'US',
+    'platform_id' => $mf->user->getPlatformDesktop(),
+    'vip' => 0,
+    'photo' => 'http://example.com/somephotourl.jpg',
+];
+
+$data = [ // Data for letter
+    'some' => 'hi',
+    'letter' => 'John',
+    'variables' => '!',
+];
+
+$meta = []; // Your additional data
+
+$response = $mf->push->send($typeId, $categoryId, $projectId, $email, $user, $data, $meta);
+var_dump($response);
+
+// Check email
+$result = $mf->email->check('test@example.com');
+var_dump($result);

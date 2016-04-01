a0f7ee89ce55bc822fab7cd87b31bbfaea0537e3
Andrey Reinwald <andreinwald@gmail.com>
2016-04-01 14:21:28 +0300

    Added pseudo-DI

---
+++ mailfire/Di.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Class MailfireDi
+ *
+ * @property \MailfirePush push
+ * @property \MailfireRequest request
+ *
+ */
+
+class MailfireDi
+{
+    public $di = null;
+
+    public function __construct($di)
+    {
+        $this->di = $di;
+    }
+
+    public function __get($propertyName)
+    {
+        if (isset($this->di->{$propertyName})) {
+            $service = $this->di->{$propertyName};
+            $this->{$propertyName} = $service;
+            return $service;
+        } else {
+            return null;
+        }
+    }
+}

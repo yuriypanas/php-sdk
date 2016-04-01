13fcd1812386c1620e5cfbffbe6c9d64e12de113
Andrey Reinwald <andreinwald@gmail.com>
2016-04-01 14:42:06 +0300

    Added Mailfire init class

---
+++ mailfire/Mailfire.php
@@ -0,0 +1,22 @@
+<?php
+
+class Mailfire
+{
+    public $clientId;
+    public $clientKey;
+
+    const CATEGORY_SYSTEM = 1;
+    const CATEGORY_TRIGGER = 2;
+
+    public function __construct($clientId, $clientKey)
+    {
+        $this->clientId = $clientId;
+        $this->clientKey = $clientKey;
+
+        $this->request = new MailfireRequest($this);
+        $this->push = new MailfirePush($this);
+        $this->email = new MailfireEmail($this);
+    }
+}
+
+

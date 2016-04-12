4c1f832c4b83e3cd8770e243f89926143293cebd
Andrey Reinwald <andreinwald@gmail.com>
2016-04-12 15:58:30 +0300

    Moved Request to new sign, add update and delete methods

--- mailfire/MailfireRequest.php
+++ mailfire/MailfireRequest.php
@@ -6,35 +6,51 @@ class MailfireRequest extends MailfireDi
 {
     const API_BASE = 'https://api.mailfire.io/v1/';
 
-    public function receive($resource, $data) {
+    public function receive($resource, $data = [])
+    {
         return $this->send($resource, 'GET', $data);
     }
 
-    public function create($resource, $data) {
+    public function create($resource, $data = [])
+    {
         return $this->send($resource, 'POST', $data);
     }
 
-    private function send($resource, $method, $data)
+    public function update($resource, $data)
+    {
+        return $this->send($resource, 'PUT', $data);
+    }
+
+    public function delete($resource)
+    {
+        return $this->send($resource, 'DELETE');
+    }
+
+    private function send($resource, $method, $data = [])
     {
         $resource = strtolower($resource);
         $method = strtoupper($method);
         $uri = self::API_BASE . $resource;
 
         $headers = [];
-        ksort($data);
-        $sign = hash_hmac('sha256', json_encode($data), $this->clientKey);
-        $data['hash'] = $sign;
-//        $headers[] = 'Authorization: Sign ' . $sign;
+        $sign = $this->getSign($uri, $method, $data);
+        $headers[] = 'Authorization: Sign ' . $sign;
 
+        return $this->sendCurl($uri, $method, $data, $headers);
+    }
+
+    private function sendCurl($uri, $method, $data, $headers)
+    {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $uri);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        if (count($data)) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        }
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
         $result = curl_exec($ch);
         curl_close($ch);
-
         $result = json_decode($result, true);
         if (!$result) {
             return false;
@@ -47,7 +63,7 @@ class MailfireRequest extends MailfireDi
         $data['request_method'] = $method;
         $data['request_uri'] = $uri;
         ksort($data);
-        $sign = hash_hmac('sha256', json_encode($data, JSON_UNESCAPED_SLASHES), $this->clientKey);
+        $sign = hash_hmac('sha256', json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE), $this->clientKey);
 
         $signData = json_encode(['client_id' => $this->clientId, 'sign' => $sign]);
         return base64_encode($signData);

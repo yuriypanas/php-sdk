cf2aad1f9551afe39c3e658ac0e6beec4cfcd3e7
Oleksandr Mykhailiuta <ercling@gmail.com>
2016-11-18 15:54:27 +0200

    Added webpush send methods

--- mailfire/MailfireWebPush.php
+++ mailfire/MailfireWebPush.php
@@ -61,13 +61,63 @@ class MailfireWebPush extends MailfireDi
     }
 
     /**
+     * array['title']   string (required) Webpush title
+     *      ['url']     string (required) Webpush link url
+     *      ['icon']    string (required) Webpush icon image url
+     *      ['type_id'] int (required) Webpush type ID
+     *      ['meta']    array (optional) Webpush meta
+     *
+     * @param array|int $user
+     * @param array $webpushMessage (See above)
+     * @return bool
+     */
+    public function sendByUser($user, array $webpushMessage)
+    {
+        $pushUser = $this->getPushUserByUser($user);
+        if (!$pushUser || !$pushUser['id']) {
+            $this->errorHandler->handle(new Exception('PushUser was not found'));
+            return false;
+        }
+
+        $webpushMessage['push_user_id'] = $pushUser['id'];
+
+        $result = $this->request->create('webpush/send', $webpushMessage);
+        return !empty($result['result']) ? $result['result'] : false;
+    }
+
+    /**
+     * array['title']   string (required) Webpush title
+     *      ['url']     string (required) Webpush link url
+     *      ['icon']    string (required) Webpush icon image url
+     *      ['type_id'] int (required) Webpush type ID
+     *      ['meta']    array (optional) Webpush meta
+     *
+     * @param $projectId
+     * @param $hash
+     * @param array $webpushMessage
+     */
+    public function sendByProjectIdAndHash($projectId, $hash, array $webpushMessage)
+    {
+        $pushUser = $this->getPushUserByProjectIdAndHash($projectId, $hash);
+        if (!$pushUser || !$pushUser['id']) {
+            $this->errorHandler->handle(new Exception('PushUser was not found'));
+            return false;
+        }
+
+        $webpushMessage['push_user_id'] = $pushUser['id'];
+
+        $result = $this->request->create('webpush/send', $webpushMessage);
+        return !empty($result['result']) ? $result['result'] : false;
+    }
+
+    /**
      * @param array|int $user
      * @return bool
      */
     protected function getPushUserByUser($user)
     {
         if (is_int($user)) {
-            $user['id'] = $user;
+            $user = ['id' => $user];
         }
 
         if (!$user || !isset($user['id']) || !$user['id']) {

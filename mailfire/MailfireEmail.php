0c6c5f94fd3d9f46f24eb421d5e7f649038e17a4
Oleksandr Mykhailiuta <mykhailiuta.aleksandr@corp.mailfire.io>
2018-03-19 20:20:13 +0200

    Add sanitize option to email check method

--- mailfire/MailfireEmail.php
+++ mailfire/MailfireEmail.php
@@ -4,9 +4,11 @@ class MailfireEmail extends MailfireDi
 {
     const CHECK_EMAIL_RESOURCE = 'email/check';
 
-    public function check($email)
+    public function check($email, $sanitize = true)
     {
-        return $this->request->create(self::CHECK_EMAIL_RESOURCE, array('email' => $email));
+        return $this->request->create(self::CHECK_EMAIL_RESOURCE, array(
+            'email' => $email,
+            'sanitize' => $sanitize));
     }
 
     public function validate($projectId, $email, $typeId)
